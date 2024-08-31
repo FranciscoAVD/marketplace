e6a5f8995050f70e878c71466bae32ef244d5596
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-08-31 17:02:16 -0400

    added cards to main dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,10 @@
 export default function page() {
-  return <main className="">Browse All</main>;
+  return (
+    <main className="bg-neutral-100 p-4 space-y-8 overflow-y-scroll">
+      <h1 className="text-xl font-bold">Today's picks</h1>
+      <div className="flex justify-center flex-wrap gap-4">
+        {Array.from({length: 49}, (_, idx) => (<div className="w-[200px] h-[250px] bg-neutral-200 rounded-lg"></div>))}
+      </div>
+    </main>
+  );
 }
