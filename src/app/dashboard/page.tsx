cb6c2d91791e498f572c39bd1eeaaed8d72ec188
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-08-31 21:59:56 -0400

    added semantic html

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function page() {
   return (
     <main className="bg-neutral-100 p-4 space-y-8 overflow-y-scroll">
       <h1 className="text-xl font-bold">Today's picks</h1>
-      <div className="flex justify-center flex-wrap gap-4">
-        {Array.from({length: 49}, (_, idx) => (<div className="w-[200px] h-[250px] bg-neutral-200 rounded-lg"></div>))}
-      </div>
+      <ul className="flex justify-center flex-wrap gap-4">
+        {Array.from({ length: 49 }, (_, idx) => (
+          <li key={idx}>
+            <Link href="/dashboard">
+              <div className="w-[200px] h-[250px] bg-neutral-200 rounded-lg"></div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
